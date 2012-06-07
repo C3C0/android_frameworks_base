2a6268fc3e844f5db84d9bd8363847eec0cd2d74
Peter Gregus <pete.gregus@gmail.com>
2012-06-07 10:59:06 +0200

    libmedia: Changed scaling of volume to enable higher volume on lower levels
    Change-Id: Ib1208bbec4aa913717ebe651dffcefcba6cec064

--- media/libmedia/AudioSystem.cpp
+++ media/libmedia/AudioSystem.cpp
@@ -196,7 +196,7 @@ String8 AudioSystem::getParameters(audio_io_handle_t ioHandle, const String8& ke
 // convert volume steps to natural log scale
 
 // change this value to change volume scaling
-static const float dBPerStep = 0.5f;
+static const float dBPerStep = 0.25f;
 // shouldn't need to touch these
 static const float dBConvert = -dBPerStep * 2.302585093f / 20.0f;
 static const float dBConvertInverse = 1.0f / dBConvert;
